122c20b393143ad52183348171cea74e758e3814
xenia <xenia@Polyxenis-MacBook-Pro.local>
2020-05-06 00:12:17 +0300

    Add findByPostalCode method

--- src/main/java/com/fab/repositories/gr/BailiffRepository.java
+++ src/main/java/com/fab/repositories/gr/BailiffRepository.java
@@ -1,12 +1,13 @@
 package com.fab.repositories.gr;
 
+import java.util.List;
+
 import com.fab.models.gr.BailiffEntity;
-// import com.fab.models.gr.BailiffResponse;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("bailiffRepository")
 public interface BailiffRepository extends JpaRepository<BailiffEntity, Long> {
-    //  BailiffEntity findByPostalCode(String postalCode);
+     List<BailiffEntity> findByPostalCode(String postalCode);
 }
--- src/main/java/com/fab/services/gr/BailiffService.java
+++ src/main/java/com/fab/services/gr/BailiffService.java
@@ -16,34 +16,14 @@ public class BailiffService {
     @Autowired
     private BailiffRepository bailiffRepository;
     
-    // public BailiffEntity findBailiffByPostalCode(String postalCode) {
-    //     return memberRepository.findByPostalCode(postalCode);
-    // }
-    
+    public BailiffResponse findBailiffByPostalCode(String country, String lang, String postalCode) {
+        List<BailiffEntity> bailiffs = bailiffRepository.findByPostalCode(postalCode);
+        return new BailiffResponse(bailiffs, country, lang);
+    }
+
     public BailiffResponse getAllBailiffs(String country, String lang) {
         List<BailiffEntity> bailiffs = bailiffRepository.findAll();
         return new BailiffResponse(bailiffs, country, lang);
     }
 
-    // public BailiffEntity saveMember(BailiffEntity bailiff) {
-    //     return bailiffRepository.save(bailiff);
-    // }
-
-    // public BailiffEntity updateMember(BailiffEntity bailiff, Long id) {
-    //     BailiffEntity updateBailiff = bailiffRepository.findById(id).orElse(null);
-    //     if(updateBailiff != null) {
-    //         updateBailiff.setName(bailiff.getName());
-    //      }
-    //     final BailiffEntity myBailiff = bailiffRepository.save(updateBailiff);
-    //     return myBailiff;
-    //   }
-
-    //   public Boolean deleteBailiff (Long id) {
-    //     BailiffEntity delBailiff  = bailiffRepository.findById(id).orElse(null);
-    //     if(delBailiff != null) {
-    //         bailiffRepository.delete(delBailiff);
-    //         return true;
-    //     }
-    //     return false;
-    //  }
 }
